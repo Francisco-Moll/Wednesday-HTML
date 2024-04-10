bc98d16edb4f7da82fd2af7a0bf2072739696446
Francisco M. <fmoll1999@gmail.com>
2024-04-10 13:43:37 -0400

    Added js

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+//Object.Method parameters (EVENT_NAME, HANDLER_FUNCTION...)
+document.addEventListener("DOMContentLoaded", ()=>{
+
+    //DOM -- Document Object Model
+    extra_special.innerHTML = "Repacement Text!";
+    extra_special.style.color = "blue";
+
+}); //End DOMContentLoaded
